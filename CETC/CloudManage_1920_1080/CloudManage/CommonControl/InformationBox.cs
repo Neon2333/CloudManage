868f790351041c27c649c254498b45faf8899ff1
WangKun19920124 <2522466153@qq.com>
2021-12-24 14:44:03 +0800

    1.编写infoBox；2.将动态库整理到dll文件夹中

---
+++ CETC/CloudManage_1920_1080/CloudManage/CommonControl/InformationBox.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudManage.CommonControl
+{
+    public partial class InformationBox : DevExpress.XtraEditors.XtraUserControl
+    {
+        public InformationBox()
+        {
+            InitializeComponent();
+            this.timer_disappear.Enabled = true;
+        }
+
+        public string infoTitle
+        {
+            set
+            {
+                this.labelControl_infoTitle.Text = "      " + value;
+            }
+            get
+            {
+                return this.labelControl_infoTitle.Text.Substring(6);
+            }
+        }
+
+        public int timeDisappear
+        {
+            set
+            {
+                this.timer_disappear.Interval = value;
+            }
+            get
+            {
+                return this.timer_disappear.Interval;
+            }
+        }
+
+        private void simpleButton_infoOK_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void timer_disappear_Tick(object sender, EventArgs e)
+        {
+            this.timer_disappear.Enabled = false;
+            this.Dispose();
+        }
+    }
+}
